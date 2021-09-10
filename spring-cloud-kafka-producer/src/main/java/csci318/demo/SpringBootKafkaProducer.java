eddeab53e8f4d7fc397ef8ff33066ab31bf4e1e2
gxshub <guoxin2009@gmail.com>
2021-09-10 17:42:38 +1000

    refactor

--- spring-cloud-kafka-producer/src/main/java/csci318/demo/SpringBootKafkaProducer.java
+++ spring-cloud-kafka-producer/src/main/java/csci318/demo/SpringBootKafkaProducer.java
@@ -2,32 +2,20 @@ package csci318.demo;
 
 import csci318.demo.binder.Publisher;
 import csci318.demo.model.Appliance;
-import csci318.demo.binder.binding.InOutBound;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-//@EnableBinding(InOutBound.class)
 public class SpringBootKafkaProducer {
 
 	private static final Logger log = LoggerFactory.getLogger(SpringBootKafkaProducer.class);
 	private static final String url = "https://random-data-api.com/api/appliance/random_appliance";
-
-	/*
-	@Autowired
-	private Publisher publisher;
-
-	 */
-
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootKafkaProducer.class, args);
@@ -43,16 +31,10 @@ public class SpringBootKafkaProducer {
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
 		return builder.build();
 	}
-	/*
-	@Bean
-	public Publisher publisher() {
-		return new Publisher();
-	}
-
-	 */
 
 	@Bean
-	public CommandLineRunner run(RestTemplate restTemplate, Publisher publisher) throws Exception {
+	public CommandLineRunner run(RestTemplate restTemplate, Publisher publisher)
+			throws Exception, InterruptedException {
 		return args -> {
 			//get 10 quotes
 			for (int i=0; i<10; i++) {
@@ -64,20 +46,5 @@ public class SpringBootKafkaProducer {
 		};
 	}
 
-	/*
-	@Bean
-	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
-		return args -> {
-			//get 10 quotes
-			for (int i=0; i<10; i++) {
-				Appliance appliance = restTemplate.getForObject(url, Appliance.class);
-				assert appliance != null;
-				log.info(appliance.toString());
-				publisher.publish(appliance);
-			}
-		};
-	}
-
-	 */
 
 }
--- spring-cloud-kafka-producer/src/main/java/csci318/demo/binder/Publisher.java
+++ spring-cloud-kafka-producer/src/main/java/csci318/demo/binder/Publisher.java
@@ -2,22 +2,20 @@ package csci318.demo.binder;
 
 import csci318.demo.binder.binding.InOutBound;
 import csci318.demo.model.Appliance;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.integration.support.MessageBuilder;
-import org.springframework.stereotype.Component;
 
-@Component
+
 @EnableBinding(InOutBound.class)
 public class Publisher {
 
+    @Autowired
     private InOutBound inOutBound;
 
-    public Publisher(InOutBound inOutBound) {
-        this.inOutBound = inOutBound;
-    }
-
     public void publish(Appliance appliance) {
-        this.inOutBound.outbound().send(MessageBuilder.withPayload(appliance).build());
+        inOutBound.outbound().send(MessageBuilder
+                .withPayload(appliance).build());
     }
 
 }
