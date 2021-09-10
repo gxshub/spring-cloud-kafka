0b39984e6af8c07bb9e9f8b6317d7b684d77d4f8
gxshub <guoxin2009@gmail.com>
2021-09-10 10:45:01 +1000

    refactor, remove autowired

--- spring-cloud-kafka-consumer/src/main/java/csci318/demo/SpringBootKafkaConsumer.java
+++ spring-cloud-kafka-consumer/src/main/java/csci318/demo/SpringBootKafkaConsumer.java
@@ -1,7 +1,8 @@
 package csci318.demo;
 
+import csci318.demo.binder.binding.BoundName;
+import csci318.demo.binder.binding.InOutBound;
 import csci318.demo.model.Appliance;
-import csci318.demo.service.Bindings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,17 +10,17 @@ import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 
 @SpringBootApplication
-@EnableBinding(Bindings.class)
+@EnableBinding(InOutBound.class)
 public class SpringBootKafkaConsumer {
 
-    @Autowired
-    private Bindings bindings;
+    //@Autowired
+    //private InOutBound inOutBound;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootKafkaConsumer.class, args);
     }
 
-    @StreamListener("inbound1")
+    @StreamListener(BoundName.INBOUND)
     public void listen(Appliance appliance) {
 
         System.out.println(appliance.toString());
