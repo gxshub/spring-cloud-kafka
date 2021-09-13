d93720d46653b654df69b1767a717d1837e958c6
gxshub <guoxin2009@gmail.com>
2021-09-13 15:57:35 +1000

    runnable version

--- spring-cloud-kafka-processor/src/main/java/csci318/demo/service/BrandCount.java
+++ spring-cloud-kafka-processor/src/main/java/csci318/demo/service/BrandCount.java
@@ -1,7 +1,6 @@
 package csci318.demo.service;
 
 import csci318.demo.binding.BinderRegister;
-import csci318.demo.binding.InOnlyBinder;
 import csci318.demo.binding.InOutBinder;
 import csci318.demo.model.Appliance;
 import org.apache.kafka.streams.KeyValue;
@@ -11,30 +10,24 @@ import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.handler.annotation.SendTo;
 
-@EnableBinding({InOnlyBinder.class, InOutBinder.class})
-public class BrandCount {
 
+@EnableBinding(InOutBinder.class)
+public class BrandCount {
 
     @StreamListener(BinderRegister.INBOUND)
     @SendTo(BinderRegister.OUTBOUND)
-    public KStream<String, Long> process(KStream<Object, Appliance> applianceKStream) {
-        return applianceKStream.
-                map((key,appliance) ->{
+    public KStream<String, Long> process(KStream<Object, Appliance> applianceStream) {
+
+        KStream<String, Long> brandCountStream = applianceStream.
+                map((key,appliance) -> {
                     String newkey = Integer.toString(appliance.getId());
                     String value = appliance.getBrand();
                     return KeyValue.pair(newkey, value);
                 }).
-                groupBy((keyIgnored, value)->value).count().toStream();
+                groupBy((keyIgnored, value) -> value).count().toStream();
+        brandCountStream.print(Printed.<String, Long>toSysOut().withLabel("brand count"));
+        return brandCountStream;
     }
 
-    @StreamListener(BinderRegister.INBOUND)
-    public void print(KStream<?, Appliance> applianceKStream) {
-        applianceKStream.
-                map((key,appliance) ->{
-                    String newkey = Integer.toString(appliance.getId());
-                    String value = appliance.getBrand();
-                    return KeyValue.pair(newkey, value);
-                }).
-                groupBy((keyIgnored, value)->value).count().toStream().print(Printed.toSysOut());
-    }
+
 }
--- spring-cloud-kafka-processor/src/main/java/csci318/demo/service/TopBrand.java
+++ spring-cloud-kafka-processor/src/main/java/csci318/demo/service/TopBrand.java
@@ -1,16 +1,8 @@
 package csci318.demo.service;
 
-import csci318.demo.binding.BinderRegister;
-import csci318.demo.binding.InOnlyBinder;
-import csci318.demo.model.Appliance;
-import org.apache.kafka.streams.KeyValue;
-import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.Printed;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.StreamListener;
-
-@EnableBinding(InOnlyBinder.class)
+//@EnableBinding(InOnlyBinder.class)
 public class TopBrand {
+    /*
     @StreamListener(BinderRegister.INBOUND)
     public void process(KStream<?, Appliance> applianceKStream) {
         applianceKStream.
@@ -20,6 +12,8 @@ public class TopBrand {
                     return KeyValue.pair(newkey, value);
                 }).
                 groupBy((keyIgnored, value)->value).count().toStream().print(Printed.toSysOut());
-        /* TODO */
+
     }
+
+     */
 }
