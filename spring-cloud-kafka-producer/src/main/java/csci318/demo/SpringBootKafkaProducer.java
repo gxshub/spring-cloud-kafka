5ee1cf0fd28e4b6231ccb37148bfa70a93d230f6
gxshub <guoxin2009@gmail.com>
2021-09-14 09:25:41 +1000

    move producer function to SpringBootKafkaProducer

--- spring-cloud-kafka-producer/src/main/java/csci318/demo/SpringBootKafkaProducer.java
+++ spring-cloud-kafka-producer/src/main/java/csci318/demo/SpringBootKafkaProducer.java
@@ -1,6 +1,6 @@
 package csci318.demo;
 
-import csci318.demo.service.AppliancePublisher;
+import csci318.demo.binding.OutBinding;
 import csci318.demo.model.Appliance;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,10 +8,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.context.annotation.Bean;
+import org.springframework.integration.support.MessageBuilder;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableBinding(OutBinding.class)
 public class SpringBootKafkaProducer {
 
 	private static final Logger log = LoggerFactory.getLogger(SpringBootKafkaProducer.class);
@@ -33,14 +36,14 @@ public class SpringBootKafkaProducer {
 	}
 
 	@Bean
-	public CommandLineRunner run(RestTemplate restTemplate, AppliancePublisher publisher) throws Exception {
+	public CommandLineRunner run(RestTemplate restTemplate, OutBinding outBinding) throws Exception {
 		return args -> {
 			try{
 				while(!Thread.currentThread().isInterrupted()){
 					Appliance appliance = restTemplate.getForObject(url, Appliance.class);
 					assert  appliance != null;
 					log.info(appliance.toString());
-					publisher.publish(appliance);
+					outBinding.outbound().send(MessageBuilder.withPayload(appliance).build());
 					Thread.sleep(1200);
 				}
 			}
@@ -48,6 +51,4 @@ public class SpringBootKafkaProducer {
 			}
 		};
 	}
-
-
 }
