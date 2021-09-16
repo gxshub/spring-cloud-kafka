ae5e45b4429188ae502b371d3bee0d6b60568c56
gxshub <guoxin2009@gmail.com>
2021-09-16 23:33:48 +1000

    use StreamBridge to send message

--- spring-cloud-kafka-producer/src/main/java/csci318/demo/SpringBootKafkaProducer.java
+++ spring-cloud-kafka-producer/src/main/java/csci318/demo/SpringBootKafkaProducer.java
@@ -1,6 +1,5 @@
 package csci318.demo;
 
-import csci318.demo.binding.ProducerBinding;
 import csci318.demo.model.Appliance;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,13 +7,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.context.annotation.Bean;
-import org.springframework.integration.support.MessageBuilder;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableBinding(ProducerBinding.class)
 public class SpringBootKafkaProducer {
 
 	private static final Logger log = LoggerFactory.getLogger(SpringBootKafkaProducer.class);
@@ -24,31 +21,25 @@ public class SpringBootKafkaProducer {
 		SpringApplication.run(SpringBootKafkaProducer.class, args);
 	}
 
-	// With @Bean annotation of the following method, a RestTemplate object is injected.
-	// But there is no need to define a separate method to return a RestTemplateBuilder object.
-	// This is due to the following reason:
-	// "In a typical auto-configured Spring Boot application
-	// this builder is available as a bean and can be injected
-	// whenever a RestTemplate is needed." -- from RestTemplateBuilder API Doc
 	@Bean
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
 		return builder.build();
 	}
 
 	@Bean
-	public CommandLineRunner run(RestTemplate restTemplate, ProducerBinding binding) throws Exception {
+	public CommandLineRunner run(RestTemplate restTemplate, StreamBridge streamBridge) throws Exception {
 		return args -> {
-			try{
-				while(!Thread.currentThread().isInterrupted()){
+			try {
+				while (!Thread.currentThread().isInterrupted()){
 					Appliance appliance = restTemplate.getForObject(url, Appliance.class);
-					assert  appliance != null;
+					assert appliance != null;
 					log.info(appliance.toString());
-					binding.outbound().send(MessageBuilder.withPayload(appliance).build());
+					//The binder name "appliance-outbound" is defined in the application.yml.
+					streamBridge.send("appliance-outbound", appliance);
 					Thread.sleep(1200);
 				}
 			}
-			catch(InterruptedException ignored){
-			}
+			catch(InterruptedException ignored){}
 		};
 	}
 }
